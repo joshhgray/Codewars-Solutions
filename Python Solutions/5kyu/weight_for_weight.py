df180d635579a7c5c2e1bdb058ecbf46a8262368
Joshua Gray <71195806+joshhgray@users.noreply.github.com>
2024-09-30 15:37:08 -0400

    Update weight_for_weight.py

--- Python Solutions/5kyu/weight_for_weight.py
+++ Python Solutions/5kyu/weight_for_weight.py
@@ -1,12 +1,34 @@
-def wtest(s):
+def order_weight(s):
+    s = s.split(" ")
+    s = list(filter(None, s))
+    
+    for i, n in enumerate(s[1:]):
+        idx = i
+        while idx >= 0:
+            if get_weight(int(n)) <= get_weight(s[idx]):
+                if get_weight(int(n)) == get_weight(s[idx]):
+                    if sorted([str(int(n)), str(s[idx])]) == [str(int(n)), str(s[idx])]:
+                        swap(s, idx+1, idx)
+                    else: 
+                        idx -= 1
+                        break
+                    idx -= 1
+                else:
+                    swap(s, idx+1, idx)
+                    idx -= 1
+            else: break
+            
+    return " ".join(s)
+          
+def get_weight(num):
+    sum = 0
+    lnum = list(str(num))
+    for n in lnum:
+        sum += int(n)
+    return sum
 
-	s = s.split(" ")
-	s = list(filter(None, s))	
-	
-	for n in s[1:]:
-		print(n)
+def swap(l, a, b):
+    temp = l[a]
+    l[a] = l[b]
+    l[b] = temp
 
-
-string = '     65 99 53 22 61 100'
-
-wtest(string)
