99aa4bd30ef47af2c09e924000d907f364f58b48
Joshua Gray <71195806+joshhgray@users.noreply.github.com>
2024-10-02 19:21:31 -0400

    refactor who_likes_it.py

--- Python Solutions/6kyu/who_likes_it.py
+++ Python Solutions/6kyu/who_likes_it.py
@@ -1,17 +1,18 @@
 def likes(names):
     
     res = "no one likes this"
+    n = len(names)
     
-    if len(names) == 1:
+    if n == 1:
         res = f"{names[0]} likes this"
     
-    elif len(names) == 2:
+    elif n == 2:
         res = f"{names[0]} and {names[1]} like this"
     
-    elif len(names) == 3:
+    elif n == 3:
         res = f"{names[0]}, {names[1]} and {names[2]} like this"
 
-    elif len(names) >= 4:
+    elif n >= 4:
         res = f"{names[0]}, {names[1]} and {len(names) - 2} others like this"
         
     return res
